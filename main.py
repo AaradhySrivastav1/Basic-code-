71ad5391f5cea60c15ff08cfba8efa66ff9bd564
Aaradhy Srivastava <aaradhysrivastav2908@gmail.com>
2026-01-30 18:41:11 +0530

    Simplify add function response
    Removed personal information from the response.

--- main.py
+++ main.py
@@ -4,5 +4,6 @@ app = FastAPI()
 
 @app.get("/")
 def add():
-    return {"result": 2 + 2, "my name is Aaradhy Srivastava.. I am Btech studnet... and i am learning AIML "}
+    return {"result": 2 + 2}
+
 

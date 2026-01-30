2a52c4bbb1b29e13bff268fe73d7b03b0656de46
Aaradhy Srivastava <aaradhysrivastav2908@gmail.com>
2026-01-30 18:39:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,5 @@ app = FastAPI()
 
 @app.get("/")
 def add():
-    return {"result": 2 + 2}
+    return {"result": 2 + 2, "my name is Aaradhy Srivastava.. I am Btech studnet... and i am learning AIML "}
+
